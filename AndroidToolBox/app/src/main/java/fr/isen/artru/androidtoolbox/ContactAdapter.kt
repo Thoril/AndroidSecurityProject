ccee2dfcee705ac335d98c0702bd64a21603f87f
Unknown <marionbouteille83@gmail.com>
2019-02-07 10:15:25 +0100

    Fix imageCircle Transformation

--- AndroidToolBox/app/src/main/java/fr/isen/artru/androidtoolbox/ContactAdapter.kt
+++ AndroidToolBox/app/src/main/java/fr/isen/artru/androidtoolbox/ContactAdapter.kt
@@ -19,7 +19,7 @@ class ContactAdapter(private val dataSet: ArrayList<ContactData>, internal var m
 
     }
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var convertView = convertView
+        var myConvertView = convertView
         // Get the data item for this position
         val dataModel = getItem(position)
         // Check if an existing view is being reused, otherwise inflate the view
@@ -27,26 +27,26 @@ class ContactAdapter(private val dataSet: ArrayList<ContactData>, internal var m
 
         val result: View
 
-        if (convertView == null) {
+        if (myConvertView == null) {
 
             viewHolder = ViewHolder()
             val inflater = LayoutInflater.from(context)
-            convertView = inflater.inflate(R.layout.contact_adapter, parent, false)
-            viewHolder.txtName = convertView!!.findViewById(R.id.nom)
-            viewHolder.txtNumber = convertView!!.findViewById(R.id.tel)
-            result = convertView
+            myConvertView = inflater.inflate(R.layout.contact_adapter, parent, false)
+            viewHolder.txtName = myConvertView!!.findViewById(R.id.nom)
+            viewHolder.txtNumber = myConvertView.findViewById(R.id.tel)
+            result = myConvertView
 
-            convertView.tag = viewHolder
+            myConvertView.tag = viewHolder
         } else {
-            viewHolder = convertView.tag as ViewHolder
-            result = convertView
+            viewHolder = myConvertView.tag as ViewHolder
+            result = myConvertView
         }
 
         viewHolder.txtName!!.text = dataModel!!.name
         viewHolder.txtNumber!!.text = dataModel.number
 
         // Return the completed view to render on screen
-        return convertView
+        return result
     }
     override fun onClick(v: View?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
