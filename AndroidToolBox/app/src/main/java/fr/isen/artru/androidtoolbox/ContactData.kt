d0a59b17cd2bb14003a3a6981fb113a370165848
billaud <billaudwilliam@gmail.com>
2019-02-18 09:14:04 +0100

    Google api store

--- AndroidToolBox/app/src/main/java/fr/isen/artru/androidtoolbox/ContactData.kt
+++ AndroidToolBox/app/src/main/java/fr/isen/artru/androidtoolbox/ContactData.kt
@@ -1,14 +1,3 @@
 package fr.isen.artru.androidtoolbox
 
-class ContactData {
-
-    var name : String=""
-    var number :String=""
-
-
-    constructor(name:String,number:String) {
-        this.name=name
-        this.number=number
-    }
-
-}
+class ContactData(var name: String, var number: String)
