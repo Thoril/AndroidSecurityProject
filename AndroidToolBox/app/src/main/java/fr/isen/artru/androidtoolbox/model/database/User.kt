e4c1c46fa354b0ec67101c2ab91793d20b9bd51c
Unknown <artru.thomas.monchal@gmail.com>
2019-02-19 10:28:18 +0100

    Insert User into Databse with Cipher password

---
+++ AndroidToolBox/app/src/main/java/fr/isen/artru/androidtoolbox/model/database/User.kt
@@ -0,0 +1,13 @@
+package fr.isen.artru.androidtoolbox.model.database
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity
+data class User(
+    @PrimaryKey var uid: Int,
+    @ColumnInfo(name = "first_name") var firstName: String?,
+    @ColumnInfo(name = "last_name") var lastName: String?,
+    @ColumnInfo(name = "password") var password: String?
+)
