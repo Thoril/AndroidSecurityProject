ee415e5e50a641a6ccb95dcfb0b461c5bc67ec02
billaud <billaudwilliam@gmail.com>
2019-02-20 20:57:55 +0100

    Chiffrement login shared pref

--- AndroidToolBox/app/src/main/java/fr/isen/artru/androidtoolbox/CipherWrapper.kt
+++ AndroidToolBox/app/src/main/java/fr/isen/artru/androidtoolbox/CipherWrapper.kt
@@ -1,15 +1,45 @@
 package fr.isen.artru.androidtoolbox
 
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
 import android.util.Base64
 import java.security.Key
+import java.security.KeyStore
 import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
 import javax.crypto.spec.GCMParameterSpec
 import javax.crypto.spec.IvParameterSpec
 
-class CipherWrapper() {
+class CipherWrapper {
 
     companion object {
         const val IV_SEPARATOR = "]"
+
+
+        fun getKey(alias: String,createIfAbsent : Boolean = true): SecretKey? {
+            val keyStore = KeyStore.getInstance("AndroidKeyStore")
+            keyStore.load(null)
+            if (!keyStore.containsAlias(alias)) {
+                if(createIfAbsent)
+                {
+                    val keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES,"AndroidKeyStore")
+                    val keyGenParameterSpec = KeyGenParameterSpec.Builder(
+                        alias,
+                        KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+                    )
+                        .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                        .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                        .build()
+                    keyGenerator.init(keyGenParameterSpec)
+                    keyGenerator.generateKey()
+                }else{
+                    return null
+                }
+            }
+            val secretKeyEntry =  keyStore.getEntry(alias, null) as KeyStore.SecretKeyEntry
+            return secretKeyEntry.secretKey
+        }
     }
 
     private val cipher: Cipher = Cipher.getInstance("AES/GCM/NoPadding")
@@ -30,9 +60,13 @@ class CipherWrapper() {
         return result
     }
 
-    fun decrypt(data: String, key: Key?, useInitializationVector: Boolean = false): String {
-        var encodedString: String
+    fun decrypt(data: String?, key: Key?, useInitializationVector: Boolean = false): String {
+        val encodedString: String
 
+        if(data == null)
+        {
+            return ""
+        }
         if (useInitializationVector) {
             val split = data.split(IV_SEPARATOR.toRegex())
             if (split.size != 2) throw IllegalArgumentException("Passed data is incorrect. There was no IV specified with it.")
--- AndroidToolBox/app/src/main/java/fr/isen/artru/androidtoolbox/SauvegardeActivity.kt
+++ AndroidToolBox/app/src/main/java/fr/isen/artru/androidtoolbox/SauvegardeActivity.kt
@@ -2,16 +2,12 @@ package fr.isen.artru.androidtoolbox
 
 import android.arch.persistence.room.Room
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.security.keystore.KeyGenParameterSpec
-import android.security.keystore.KeyProperties
+import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import fr.isen.artru.androidtoolbox.model.database.AppDatabase
 import fr.isen.artru.androidtoolbox.model.database.User
 import kotlinx.android.synthetic.main.activity_sauvegarde.*
-import java.security.KeyStore
-import javax.crypto.KeyGenerator
 
 
 class SauvegardeActivity : AppCompatActivity() {
@@ -20,26 +16,7 @@ class SauvegardeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sauvegarde)
 
-        val keyStore = KeyStore.getInstance("AndroidKeyStore")
-        keyStore.load(null)
-        val alias = "myKey"
-
-        if (!keyStore.containsAlias(alias)) {
-
-            val keyGenerator =KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES,"AndroidKeyStore")
-            val keyGenParameterSpec = KeyGenParameterSpec.Builder(
-                alias,
-               KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
-            )
-                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
-                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
-                .build()
-            keyGenerator.init(keyGenParameterSpec)
-            keyGenerator.generateKey()
-        }
-
-        val secretKeyEntry =  keyStore.getEntry(alias, null) as KeyStore.SecretKeyEntry
-        val secretKey = secretKeyEntry.secretKey
+        val secretKey = CipherWrapper.getKey("myKey",true)
 
         val db = Room.databaseBuilder(
             applicationContext,
@@ -48,7 +25,7 @@ class SauvegardeActivity : AppCompatActivity() {
 
         insert.setOnClickListener{
             val cypherWrapper = CipherWrapper()
-            val temp = resources.getIdentifier(firstName.text.toString(), lastName.text.toString(), this.packageName)
+            resources.getIdentifier(firstName.text.toString(), lastName.text.toString(), this.packageName)
             val user = User(firstName.text.toString(), lastName.text.toString(), cypherWrapper.encrypt(password.text.toString(),secretKey, true))
             db.userDao().insertAll(user)
             Toast.makeText(this,"Informations Enregistrées", Toast.LENGTH_SHORT).show()
